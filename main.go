8cc88641d30284a9bed45fdfa2d3bf8c2f359ac7
sinashk78 <sina.sh.k78@gmail.com>
2021-11-13 11:20:38 +0330

    added error page

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -12,11 +13,28 @@ import (
 type Server struct {
 }
 
+func handleError(w http.ResponseWriter) error {
+	t, err := template.ParseFiles(path.Join("./", "template", "error.gohtml"))
+	if err != nil {
+		return err
+	}
+
+	if err = t.Execute(w, nil); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func handler(r http.ResponseWriter, _ *http.Request) {
 	indexPath := path.Join("./", "static", "index.html")
 	f, err := os.ReadFile(indexPath)
 	if err != nil {
 		log.Printf("handler: failed to open index.html: %s", err.Error())
+		if err = handleError(r); err != nil {
+			log.Printf("handler: failed to handle error: %s", err.Error())
+		}
+		return
 	}
 
 	_, err = r.Write(f)
